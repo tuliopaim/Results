172a6faa407a8588aa71eaf0c429340e0b393cd9
tuliopaim <tutpaim@gmail.com>
2021-03-12 09:08:10 -0300

    Implement the interfaces and implements the Results classes

---
+++ TulioPaim.Results/IResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TulioPaim.Results
+{
+    public interface IResult
+    {
+        string Message { get; set; }
+
+        bool Succeeded { get; set; }
+
+        List<string> Errors { get; set; }
+    }
+
+    public interface IResult<out T> : IResult
+    {
+        T Data { get; }
+    }
+}
---
+++ TulioPaim.Results/PaginatedResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TulioPaim.Results
+{
+    public class PaginatedResult<T> : Result<IEnumerable<T>>
+    {
+        /// <summary>
+        /// Create a PaginatedResult
+        /// </summary>
+        /// <param name="data">IEnumerable<T> results</param>
+        /// <param name="total">Total items</param>
+        /// <param name="page">Page number (>=1)</param>
+        /// <param name="pageSize">Page Size</param>
+
+        public PaginatedResult(
+            IEnumerable<T> data,
+            long total,
+            int page,
+            int pageSize)
+        {
+            Data = data;
+            Total = total;
+            Page = page;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(Total / (double)PageSize);
+        }
+
+        public int PageSize { get; }
+
+        public int Page { get; }
+
+        public int TotalPages { get; }
+
+        public long Total { get; }
+
+        public bool HasPrevPage => Page > 1;
+
+        public bool HasNextPage => Page < TotalPages;
+    }
+}
