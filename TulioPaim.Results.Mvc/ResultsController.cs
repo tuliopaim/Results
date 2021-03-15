f2d20f3b2abf60587a3d13058a7e0eec42bd1368
tuliopaim <tutpaim@gmail.com>
2021-03-14 23:32:53 -0300

    Add Results.Mvc to solution with ReturnResult helper

---
+++ TulioPaim.Results.Mvc/ResultsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TulioPaim.Results.Mvc
+{
+    public class ResultsController : Controller
+    {
+        protected ActionResult<Result> ReturnResult(Result result)
+        {
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        protected ActionResult<Result<T>> ReturnResult<T>(Result<T> result)
+        {
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        protected ActionResult<ListResult<T>> ReturnListResult<T>(ListResult<T> result)
+        {
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        protected ActionResult<PaginatedResult<T>> ReturnPaginatedResult<T>(PaginatedResult<T> result)
+        {
+            if (result.Succeeded)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
